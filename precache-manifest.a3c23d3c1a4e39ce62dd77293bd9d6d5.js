f457ee5dcc747b9fdeaf194c3c0c3c946245e0ee
Ijaj Ahmed Bhuiya <iahmed1400@gmail.com>
2022-10-19 19:31:08 +0600

    Add files via upload

---
+++ precache-manifest.a3c23d3c1a4e39ce62dd77293bd9d6d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef227e1984f14faaa4c2",
+    "url": "/static/css/main.6c41dc98.chunk.css"
+  },
+  {
+    "revision": "ef227e1984f14faaa4c2",
+    "url": "/static/js/main.ef227e19.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "6894059dc7c78e860f50",
+    "url": "/static/css/2.798384fd.chunk.css"
+  },
+  {
+    "revision": "6894059dc7c78e860f50",
+    "url": "/static/js/2.6894059d.chunk.js"
+  },
+  {
+    "revision": "39a7286a869c5b2ec7f4",
+    "url": "/static/js/runtime~main.39a7286a.js"
+  },
+  {
+    "revision": "abf53d52c9f4b2b712c7a04ab1faabf6",
+    "url": "/static/media/pp - 250 X 250.abf53d52.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3bf78cb40c45d136f3f3373bfb738ff4",
+    "url": "/index.html"
+  }
+];
